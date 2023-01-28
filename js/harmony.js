369ffb5a8e2226814233facdf115e6f97dbc0777
Haruna-Kurahashi <kp.pickles.f.miffy@gmail.com>
2023-01-29 04:03:29 +0900

    ハーモニービジュアル

--- js/harmony.js
+++ js/harmony.js
@@ -22,6 +22,13 @@ let equalTonic;
 let equalDominant;
 let equalMediant;
 let adjustValue = 3;
+let harmonyVisualsL = [];
+let harmonyVisualsR = [];
+let micVisuals = [];
+let animations = [];
+let animations2 = [];
+let colors = [];
+let shadows = [];
 
 // let note3;
 // let note4;
@@ -29,7 +36,7 @@ let adjustValue = 3;
 // const majorKey = ["C", "G", "D", "A", "E", "H", "Fsharp", "Csharp", "F", "B", "Aflat", "Dflat", "Gflat", "Cflat"];
 const majorKey = ["C", "G", "D", "A", "E", "H"];
 const harmonyKey = new HarmonyKey();
-  
+
 let c1, c2;
 function setup() {
   const canvas = createCanvas(windowWidth, windowHeight);
@@ -43,41 +50,83 @@ function setup() {
   note2 = new p5.Oscillator("sine");
   noteQuiz = new p5.Oscillator("sine");
   choiceKey();
-  
-}
+  rectMode(CENTER);
 
-var vx = 0;
-function draw() {
-  background('#140d36');
-  fill(0, 20);
+  colors = [color(0, 80, 100, 80),
+            color(193, 253, 166, 50)];
+  shadows = [color(0, 0, 255),
+             color(0, 146, 69)];
   
 
-  if (gameState == true) {
-    if (maxDiff == mediantDiff) {
-      let midiantDiff = calculateScore(equalMediant, harmonyKey.mediant_tone);
-      console.log("平均律", equalMediant);
-      console.log("純正律", harmonyKey.mediant_tone);
-      console.log("マイク", micFreq);
-      let micDiff = calculateDiff(micFreq, harmonyKey.mediant_tone);
-      console.log("micDiff", mediantDiff);
-      console.log("マイク差分", micDiff);
-      if (midiantDiff > 0) {
-        if (micDiff < 2.75) {
-          console.log("パーフェクト");
-          fill(0, 0, 255);
-          ellipse(width / 2, height / 2, 500, 500);
-        } else if (micDiff < maxDiff) {
-          console.log("Good");
-          fill(255, 255, 0);
-          ellipse(width / 2, height / 2, 300, 300);
-        } else if(micDiff > maxDiff){
-          console.log("惜しい");
-          fill(255, 0, 0);
-          ellipse(width / 2, height / 2, 200, 200);
-        }
-      }
+  for (let i = 0; i < 5; i++) {
+    if (i == 0) {
+      harmonyVisualsL.push(new VisualHarmony(width / 4 + 50, height/ 2 -100, 360));
+      harmonyVisualsR.push(new VisualHarmony(width - width / 4 - 50, height / 2 - 100, 360));
+      // micVisuals.push(new VisualHarmony(width /2, height/2 - 100, 300));
+    } else {
+      let d = random(50, 360);
+      harmonyVisualsL.push(new VisualHarmony(width / 4 + 50, height / 2 - 100, d));
+      harmonyVisualsR.push(new VisualHarmony(width - width / 4 - 50, height / 2 - 100, d));
+      // micVisuals.push(new VisualHarmony(width /2, height/2 - 100, d2));
     }
   }
+  for (let i = 0; i < 5; i++) {
+    if (i == 0) {
+      micVisuals.push(new VisualHarmony(width /2, height/2 - 100, 300));
+    } else {
+      let d2 = random(20, 300);
+      micVisuals.push(new VisualHarmony(width /2, height/2 - 100, d2));
+    }
+  }
+  for (let i = 0; i < 3; i++) {
+    animations2.push(new AnimationSmallRects());
+  }
+}
+
+var count = 0;
+var vx = 0;
+function draw() {
+  blendMode(BLEND);
+  background('#140d36');
+  drawQuiz();
+
+  // push();
+  // translate(width / 2, height / 2 - 100);
+  // rotate(radians(45));
+  // stroke(255);
+  // noFill();
+  // strokeWeight(4);
+  // square(0, 0, 400);
+  // pop();
+  for (let i = 0; i < micVisuals.length; i++) {
+    micVisuals[i].draw();
+    micVisuals[i].setColor(color(193, 253, 166, 50), color(0, 146, 69));
+    // micVisuals[i].setColor(color(0, 0, 255, 100), color(0, 0, 255));
+    micVisuals[i].setBlur(0);
+  }
+  for (let i = 0; i < harmonyVisualsL.length; i++) {
+    harmonyVisualsL[i].draw();
+    harmonyVisualsR[i].draw();
+    harmonyVisualsL[i].setColor(color(193, 253, 166, 50), color(0, 146, 69));
+    harmonyVisualsR[i].setColor(color(193, 253, 166, 50), color(0, 146, 69));
+  }
+  for (let i = 0; i < animations.length; i++) {
+    animations[i].draw();
+  }
+  push();
+
+  // translate(width /2, height/2 - 100);
+  // rotate(radians(45));
+  // noFill();
+  // strokeWeight(4);
+  // stroke(color(0, 104, 55));
+  // square(0, 0, 260, 2);
+  // strokeWeight(2);
+  // square(0, 0, 220, 2);
+  // square(0, 0, 200), 2;
+  // pop();
+
+  drawMicVisual();
 
 }
 
@@ -206,7 +255,7 @@ function keyPressed() {
       timer = 5;
     }
   }
-  if (keyCode === RIGHT_ARROW) {
+  if (keyCode === RIGHT_ARROW && gameState == false) {
     if (quizCount >= quizNum - 1) {
       quizState == 'finish';
     } else {
@@ -226,42 +275,42 @@ function keyPressed() {
 }
 
 function drawQuiz() {
+  blendMode(BLEND);
   if (gameState == false) {
     if (quizState == 'before') {
       fill("#3d3a60");
-      rect(width / 2 - 110, height - 300, 220, 70, 20);
+      rect(width / 2 , height - 200, 220, 70, 20);
       textSize(30);
       fill(255);
       textFont('Mochiy Pop One');
-      text('S T A R T', width / 2 - 80, height - 250);
-    } else if (quizState == 'finish') {
-      
+      text('S T A R T', width / 2 - 80, height - 190);
     } else if (quizState == 'doing') {
       fill("#3d3a60");
-      rect(width / 2 - 100, height - 300, 200, 70, 20);
+      rect(width / 2 , height - 200, 200, 70, 20);
       textSize(32);
       fill(255);
       textFont('Mochiy Pop One');
-      text('N E X T', width / 2 - 70, height - 250);
+      text('N E X T', width / 2 - 80, height - 190);
     }
   }
   if (gameState == true) {
+    frame++;
     textSize(42);
     fill(255);
     textAlign(CENTER);
     textFont('Mochiy Pop One');
-    text(harmonyKey.quizName + "を弾こう！", width / 2 - 50, 50);
+    text(harmonyKey.quizName + "を弾こう！", width / 2, 50);
     fill("#3d3a60");
     textFont('Oswald');
-    textSize(32
-    );
+    textSize(32);
     text('T I M E', 100, height / 2 + 180);
     textSize(120);
     text(harmonyKey.keyName, width - 150, height - 130);
     textSize(130);
     text(timer, 100, height - 140);
-    frame++;
-      
+    // for (let i = 0; i < harmonyVisuals.length; i++) {
+    //   harmonyVisuals[i].draw();
+    // }
       
     if (frame > 60) {
       timer--;
@@ -274,6 +323,62 @@ function drawQuiz() {
       note1.stop();
       note2.stop();
      // noteQuiz.stop();
+    }
+  }
+}
+
+function drawMicVisual() {
+  if (gameState == true) {
+    if (maxDiff == mediantDiff) {
+      let midiantDiff = calculateScore(equalMediant, harmonyKey.mediant_tone);
+      console.log("平均律", equalMediant);
+      console.log("純正律", harmonyKey.mediant_tone);
+      console.log("マイク", micFreq);
+      let micDiff = calculateDiff(micFreq, harmonyKey.mediant_tone);
+      console.log("micDiff", mediantDiff);
+      console.log("マイク差分", micDiff);
+      if (midiantDiff > 0) {
+        if (micDiff < 2.75) {
+          for (let i = 0; i < micVisuals.length; i++) {
+            micVisuals[i].draw();
+            micVisuals[i].setBlur(80);
+          }
+          count++
+          if (count > 60) {
+            animations.push(new AnimationRectangle());
+            animations2.push(new AnimationSmallRects());
+            animations2.push(new AnimationSmallRects());
+            animations2.push(new AnimationSmallRects());
+            count = 0;
+          }
+          console.log("パーフェクト");
+          // fill(0, 0, 255);
+          // ellipse(width / 2, height / 2, 500, 500);
+        } else if (micDiff < maxDiff) {
+          console.log("Good");
+          // fill(255, 255, 0);
+          // ellipse(width / 2, height / 2, 300, 300);
+          for (let i = 0; i < micVisuals.length; i++) {
+            micVisuals[i].draw();
+            micVisuals[i].setBlur(0);
+          }
+        } else if (micDiff > maxDiff) {
+          console.log("惜しい");
+          push();
+          blendMode(SCREEN);
+          translate(width /2, height/2 - 100);
+          rotate(radians(45));
+          noFill();
+          strokeWeight(4);
+          stroke(color(0, 0, 255));
+          square(0, 0, 260, 2);
+          strokeWeight(2);
+          square(0, 0, 220, 2);
+          square(0, 0, 200), 2;
+          pop();
+  
+        }
+      }
     }
   }
 }
